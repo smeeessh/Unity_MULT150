a17997f1b5531d04727f2260605aff9d27278ab7
smeeessh <travis.jason22@gmail.com>
2025-04-27 13:59:37 -0700

    Initial Commit

---
+++ tViray_Hour10/Assets/GameCritique.cs
@@ -0,0 +1,7 @@
+1. The Game was too easy
+
+2. Increasing the size and speeof the balls and decreasing the hitbox of the goals would make this harder.
+
+3. Visuals like better textures, particle effects and sound effects would give the "wow" factor.
+
+4. Needing to position myself in the correct way to bounce the ball into the goal was fun.

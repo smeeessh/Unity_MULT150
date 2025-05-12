96ab81d46ee3df305dfb88d6fddf4596ccd81896
smeeessh <travis.jason22@gmail.com>
2025-05-11 21:11:15 -0700

    Hour 16 Assignment

---
+++ tViray_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,8 @@
+/*
+- Changed velocity over time
+- Color over lifetime changed to red-yellow gradient
+- Playback speer reduced to 0.57
+- Duration decreased to 1
+- Added light component
+- Changed collision to world
+*/

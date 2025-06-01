8a989e3d02d171ce6f296a7a37007ef07d1cb29d
smeeessh <travis.jason22@gmail.com>
2025-06-01 13:07:32 -0700

    Initial commit

---
+++ tViray_Final/Assets/Modifications.cs
@@ -0,0 +1,32 @@
+/*
+Directional Light
+- Changed sun angle
+- Intensity slightly increased
+
+Fog
+- Added slight blue fog
+
+Skybox
+- Reduced Exposure
+- Increased atmosphere thickness
+- Changed sun properties
+
+Chaos Ball
+- Velocity over time added
+- Playback speed reduced
+- Duration reduced
+- Emission increased
+
+Colored Ball
+- Added texture
+- Added colored light
+
+Goals
+- Slighly increased range
+- Slightly increased intensity
+- Increased hitbox size
+ 
+Walls
+- Replaced Walls with fence
+
+*/
